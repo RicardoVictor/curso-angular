d489ae958fdce50d249edb23890ea78685313582
Ricardo Victor <rvictorsoliveira@gmail.com>
2022-05-29 14:54:18 -0300

    working with 'for' (ngFor)

---
+++ src/app/components/list-render/list-render.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-render',
+  templateUrl: './list-render.component.html',
+  styleUrls: ['./list-render.component.css']
+})
+export class ListRenderComponent implements OnInit {
+  items = [1, 2, 3]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
