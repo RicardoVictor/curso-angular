a1b8454957453fc8ee272388fbe8d807ff29eecc
Ricardo Victor <rvictorsoliveira@gmail.com>
2022-05-29 15:17:06 -0300

    working with interfaces

---
+++ src/app/Animal.ts
@@ -0,0 +1,7 @@
+export interface Animal {
+  name: string;
+  type: string;
+  age: number;
+  showAge: boolean;
+  changeShowAge(): void;
+}
--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
-  styleUrls: ['./list-render.component.css']
+  styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  items = [1, 2, 3]
+  animals: Animal[] = [
+    {
+      name: 'scooby',
+      type: 'cachorro',
+      age: 25,
+      showAge: false,
+      changeShowAge() {
+        this.showAge = !this.showAge;
+      },
+    },
+    {
+      name: 'garfield',
+      type: 'gato',
+      age: 15,
+      showAge: false,
+      changeShowAge() {
+        this.showAge = !this.showAge;
+      },
+    },
+    {
+      name: 'pica-pau',
+      type: 'pássaro',
+      age: 50,
+      showAge: false,
+      changeShowAge() {
+        this.showAge = !this.showAge;
+      },
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
